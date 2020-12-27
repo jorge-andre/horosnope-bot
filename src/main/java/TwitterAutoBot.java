cc14084e340c4cf2c7cc3acb46bd39c0a814484d
Jorge Andre <jorge.matosandre@gmail.com>
2020-12-27 20:23:46 +0000

    Add http request and json deserializer

--- src/main/java/TwitterAutoBot.java
+++ src/main/java/TwitterAutoBot.java
@@ -1,9 +1,13 @@
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 
 import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.charset.Charset;
 
 public class TwitterAutoBot {
@@ -21,6 +25,19 @@ public class TwitterAutoBot {
             //fis = new FileInputStream("filepath");
             //isr = new InputStreamReader(fis, Charset.forName("Cp1252"));
             //br = new BufferedReader(isr);
+
+            URL url = new URL("http://ohmanda.com/api/horoscope/aquarius");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("Content-Type", "application/json");
+
+            InputStreamReader in = new InputStreamReader(connection.getInputStream());
+
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode json = objectMapper.readTree(in);
+
+            System.out.println(json.get("horoscope").asText());
+            in.close();
 
             Status firstTweet = sendTweet("test boop");
             Thread.sleep(10000);
